3cb490525914c4b29adc9068d21447b38e79c03d
Daeho <mrdh94@naver.com>
2025-02-17 13:37:48 +0900

    사용자 삭제 레파지토리 기능 추가

--- AssetAssistant/src/main/java/kr/daeho/AssetAssistant/user/repository/UserReposiory.java
+++ AssetAssistant/src/main/java/kr/daeho/AssetAssistant/user/repository/UserReposiory.java
@@ -3,7 +3,10 @@ package kr.daeho.AssetAssistant.user.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import kr.daeho.AssetAssistant.user.entity.UserEntity;
 
-public interface UserReposiory extends JpaRepository<UserEntity, Long> {    
+public interface UserReposiory extends JpaRepository<UserEntity, Long> {
     // 사용자 아이디로 DB에서 사용자 조회
     public UserEntity findByUserId(String userId);
+
+    // 사용자 아이디로 DB에서 사용자 삭제
+    public void deleteByUserId(String userId);
 }
